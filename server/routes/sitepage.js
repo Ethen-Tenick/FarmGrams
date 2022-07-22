cab9a5d67faad7b4839ee8cdb91ee50caae6be52
Ethen <ethentenick@gmail.com>
2022-07-23 00:18:17 +0300

    intro-page

--- server/routes/sitepage.js
+++ server/routes/sitepage.js
@@ -1,19 +1,25 @@
 const express = require('/data/data/com.termux/files/usr/lib/node_modules/express');
 const router = express.Router();
+const page = '/data/data/com.termux/files/home/storage/dcim/Portfolio/farmGram/client/sitepage'
+
 // the direction of the routes
+
 const login = require('./login.js');
 const register = require('./register.js');
 const profile = require('./self-profile.js');
 
 //end of the direction to the routes
 
+router.use(express.static(page));
+
 router.route('/').
 	get((req,res) => {
-		res.status(200).send('this is the sitepage');
+		res.status(200);
 	});
 
 //if user is logged in then continue to the homepage
 //and have passed in the name of the user in the url
+
 router.use('/login', login);
 
 router.use('/register', register);
